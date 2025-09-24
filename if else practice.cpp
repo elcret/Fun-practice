0b60248347fcdaaa445d4a93a4b23405f37790ce
elcret <cretel@udmercy.edu>
2025-09-24 19:03:43 -0400

    Add files via upload

---
+++ if else practice.cpp
@@ -0,0 +1,28 @@
+//if else practice
+#include <iostream>
+using namespace std;
+
+int main() {
+	int  a, b, c;
+
+	cout << "Please enter three numbers, separated by a space: " << endl;
+	cin >> a >> b >> c;
+
+	if (a > b) {
+		if (a > c) {
+			cout << a;
+		}
+		else {
+			cout << c;
+		}
+	}
+	else {
+		if (b > c) {
+			cout << b;
+		}
+		else {
+			cout << c;
+		}
+	}
+	return 0;
+}
